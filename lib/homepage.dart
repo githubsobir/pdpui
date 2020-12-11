b7d90003dfc4e4df8f691fc1775c182a8b9ff125
githubsobir <githubsobir@gmail.com>
2020-12-11 11:51:35 +0500

    start

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -4,7 +4,26 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text('UI'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'User',
+              style: TextStyle(color: Colors.red, fontSize: 35),
+            ),
+            SizedBox(width: 10,),
+            Text(
+              'Interface',
+              style: TextStyle(color: Colors.green, fontSize: 35),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
